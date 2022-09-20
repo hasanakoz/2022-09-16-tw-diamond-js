1b1980763b0cb78f808b31b545601c8108a33e8b
hasan <hasanakoz1986@gmail.com>
2022-09-20 13:49:14 +0300

    scripts created

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+const number = document.querySelector("#number1");
+const btn = document.querySelector("#draw1");
+const area = document.querySelector("#result");
 
+btn.addEventListener("click", () => {
+  drawDiamond();
+});
+
+const drawDiamond = () => {
+  let diamond = "";
+
+  for (let i = 1; i <= number.value; i++) {
+    // printing spaces
+    for (let j = number.value; j > i; j--) {
+      diamond += " ";
+    }
+    // printing star
+    for (let k = 0; k < i * 2 - 1; k++) {
+      if (k === 0 || k === 2 * i - 2) {
+        diamond += "*";
+      } else {
+        diamond += " ";
+      }
+    }
+  }
+  diamond += "\n";
+
+  // downside diamond
+  for (let i = 1; i <= number.value - 1; i++) {
+    // printing spaces
+    for (let j = 0; j < i; j++) {
+      diamond += " ";
+    }
+    // printing star
+    for (let k = (number.value - i) * 2 - 1; k >= 1; k--) {
+      if (k === 1 || k === (number.value - i) * 2 - 1) {
+        diamond += "*";
+      } else {
+        diamond += " ";
+      }
+    }
+    diamond += "\n";
+  }
+
+  area.innerHTML = diamond;
+
+  console.log(diamond);
+};
+
+// drawDiamond();
